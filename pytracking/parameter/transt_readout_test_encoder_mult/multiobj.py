fed762adf5f863a0c46f7e1e8d2f789b3f3b1fd1
Drew Linsley <drewlinsley@gmail.com>
2022-06-16 12:12:56 -0400

    Added multi object tracking.

---
+++ pytracking/parameter/transt_readout_test_encoder_mult/multiobj.py
@@ -0,0 +1,13 @@
+from pytracking.utils import TrackerParams
+from pytracking.features.net_wrappers import NetWithBackbone
+
+def parameters():
+    params = TrackerParams()
+    params.debug = 0
+    params.visualization = True
+    params.multiobj_mode = "parallel"
+    params.use_gpu = True
+    params.net = NetWithBackbone(net_path='transt_mult.pth',
+                                 use_gpu=params.use_gpu)
+    return params
+
