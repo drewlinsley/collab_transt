41dbf20eddd15aff02d0000bedb399a5587d4e6f
Drew Linsley <drewlinsley@gmail.com>
2022-06-28 18:17:19 -0400

    Fixed TransT bug.

--- pytracking/evaluation/tracker.py
+++ pytracking/evaluation/tracker.py
@@ -420,8 +420,7 @@ class Tracker:
             output_boxes.append(state)
 
             # If the tracker box confidence is < threshold, kill the tracker
-            import pdb;pdb.set_trace()
-            print(out["max_score"][1].max())
+            print({k: max(v) for k, v in out["max_score"].items()})
 
         return output_boxes
         # if save_results:
