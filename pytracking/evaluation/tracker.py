1fe6005049bbb4a2012c82bd4272daf7bcea9642
Drew Linsley <drewlinsley@gmail.com>
2022-07-01 13:19:20 -0400

    Updated.

--- pytracking/evaluation/tracker.py
+++ pytracking/evaluation/tracker.py
@@ -418,9 +418,9 @@ class Tracker:
             out = tracker.track(frame)
             state = [int(s) for s in out['target_bbox'][1]]
             # If the tracker box confidence is < threshold, kill the tracker
-            if out["max_score"][1].max() < 0.93:
+            print({k: max(v) for k, v in out["max_score"].items()}, state)
+            if out["max_score"][1].max() < 0.6:
                 return output_boxes
-            # print({k: max(v) for k, v in out["max_score"].items()}, state)
 
             output_boxes.append(state)
 
