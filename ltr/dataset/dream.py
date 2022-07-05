c411a00ebe3f3a75a8526d4f460ab56fee9a07f1
Drew Linsley <drewlinsley@gmail.com>
2022-07-05 11:12:02 -0400

    Fixing training.

--- ltr/dataset/dream.py
+++ ltr/dataset/dream.py
@@ -52,7 +52,7 @@ class Dream(BaseVideoDataset):
             file_wells = [x.split("_")[-1].replace(".npy", "") for x in files]
             annos_files = {}
             for well in file_wells:
-                data = [file_wells.well == well]
+                data = data[annotations.well == well]
 
                 # Then sort by time
                 data = data.sort_values("time")
