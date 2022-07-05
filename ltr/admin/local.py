45a91ac1097252a972df8e78487e2155f5897708
Drew Linsley <drewlinsley@gmail.com>
2022-07-05 10:55:41 -0400

    Fixing training.

--- ltr/admin/local.py
+++ ltr/admin/local.py
@@ -11,7 +11,7 @@ class EnvironmentSettings:
         self.coco_dir = ''
         self.lvis_dir = ''
         self.sbd_dir = ''
-        self.dream_dir = 'content/drive/MyDrive/tracking_cells/proc_tracking_data'
+        self.dream_dir = '/content/drive/MyDrive/tracking_cells/proc_tracking_data'
         # self.workspace_dir + '/dream/'
 
 
