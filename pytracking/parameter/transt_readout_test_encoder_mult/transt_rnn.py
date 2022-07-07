8b972517ec8639fbe68a5b7bac9cc8cbfd93d36d
Drew Linsley <drewlinsley@gmail.com>
2022-07-06 20:36:43 -0400

    Added str conversion.

--- pytracking/parameter/transt_readout_test_encoder_mult/transt_rnn.py
+++ pytracking/parameter/transt_readout_test_encoder_mult/transt_rnn.py
@@ -6,6 +6,8 @@ def parameters():
     params.debug = 0
     params.visualization = False
     params.use_gpu = True
-    params.net = NetWithBackbone(net_path='transt_rnn.pth',
+    # params.net = NetWithBackbone(net_path='transt_rnn.pth',
+    #                              use_gpu=params.use_gpu)
+    params.net = NetWithBackbone(net_path='/content/drive/MyDrive/tracking_cells/checkpoints/transt_rnn/ckpt.pth.tar',
                                  use_gpu=params.use_gpu)
     return params
