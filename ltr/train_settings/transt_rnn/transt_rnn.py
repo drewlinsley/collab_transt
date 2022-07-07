d6f129d3f8392244137bf8c6a6af952366957c34
Drew Linsley <drewlinsley@gmail.com>
2022-07-07 08:48:26 -0400

    Fixing training.

--- ltr/train_settings/transt_rnn/transt_rnn.py
+++ ltr/train_settings/transt_rnn/transt_rnn.py
@@ -74,7 +74,7 @@ def run(settings):
     transform_train = tfm.Transform(tfm.ToTensorAndJitter(0.2),
                                     tfm.RandomHorizontalFlip(),
                                     # tfm.RandomVerticalFlip(),
-                                    tfm.RandomAffine(p_flip=0.5, max_scale=1.5),
+                                    # tfm.RandomAffine(p_flip=0.5, max_scale=1.5),
                                     tfm.RandomBlur(1),
                                     tfm.Normalize(mean=settings.normalize_mean, std=settings.normalize_std))
 
